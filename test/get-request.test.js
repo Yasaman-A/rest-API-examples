335c0b3a0cb2db6870479a59e801a378972d883e
Yasaman <yamannejad@mtroyal.ca>
2022-11-24 01:02:59 -0700

    updated the endpoints

--- test/get-request.test.js
+++ test/get-request.test.js
@@ -23,23 +23,21 @@ const seedDb = () => {
 
 describe('Employee endpoint', () => {
     test("should return get result when employee exist", () => {
-        const want = [
-            { id: 66, name: "Super Test", email: "user@test.ca", phone: "243234345676", address: "43 Street SW" }
-        ]
-        return request(app).get('/api/emp/66').then(
+        const expected = [{"id":2,"name":"New User","phone":"444 444 4444","email":"new.user@test.com","address":"123 street NW"}]
+        return request(app).get('/api/employees/2').then(
             (actualValue) => {
                 expect(actualValue.status).toBe(200);
-                expect(actualValue.body).toEqual(want);
+                expect(actualValue.body).toEqual(expected);
             }
         )
     });
 
     test("should return empty result when employee does not exist", () => {
-        const want = []
-        return request(app).get('/api/emp/190').then(
+        const expected = []
+        return request(app).get('/api/employees/190').then(
             (got) => {
                 expect(got.status).toBe(200);
-                expect(got.body).toEqual(want);
+                expect(got.body).toEqual(expected);
             }
         )
     })
--- test/post-request.test.js
+++ test/post-request.test.js
@@ -17,7 +17,7 @@ describe('Employee endpoint', () => {
     test("should create a new employee on valid post requests", () => {
         const toAdd = { "name": "Super Test", "email": "user@test.ca", "phone": "243234345676", "address": "43 Street SW" }
 
-        return request(app).post('/api/emp/').send(toAdd).then(
+        return request(app).post('/api/employees/').send(toAdd).then(
             (got) => {
                 expect(got.status).toBe(200);
                 expect(got.body).toEqual({ "result": "Employee added successfully!" });
